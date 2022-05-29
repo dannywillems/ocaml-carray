a62804786bd671e689f0f73b4beeb05c5f956ed0
Danny Willems <be.danny.willems@gmail.com>
2022-05-29 23:24:45 +0200

    Simplify the interface

--- src/carray.ml
+++ src/carray.ml
@@ -1,136 +1,9 @@
-module type S = sig
-  (** The type of C arrays *)
-  type 'a t
-
-  val size_in_bytes : int
-
-  (** [allocate n] returns a fresh array of length [n] *)
-  val allocate : int -> 'a t
-
-  (** [make n x] returns a fresh array of length [n], initialized with [x].
-      Each element of the array is a fresh copy of [a]. This is different from
-      [Array.make] *)
-  val make : int -> 'a -> 'a t
-
-  (** [init n f] returns a fresh array of length [n], with element number [i]
-      initialized to the result of [f i]. In other terms, [init n f] tabulates the
-      results of [f] applied to the integers [0] to [n-1]. *)
-  val init : int -> (int -> 'a) -> 'a t
-
-  (** [of_array a] builds a C array with the same content than the Caml array [a] *)
-  val of_array : 'a array -> 'a t
-
-  (** [to_array a] returns a Caml array with the same content than the C array [a] *)
-  val to_array : 'a t -> 'a array
-
-  (** [of_list l] returns a C array with the same content than the list [l] *)
-  val of_list : 'a list -> 'a t
-
-  (** [to_list a] returns a Caml list with the same content than the array [a] *)
-  val to_list : 'a t -> 'a list
-
-  (** [set a n x] modifies array [a] in place, replacing element number [n] with
-      [x] *)
-  val set : 'a t -> 'a -> int -> unit
-
-  (** [get a n] gets an returns the element number [n] of the array [a]. The
-      first element has number [0]. The last element has number length [length -
-      1]. *)
-  val get : 'a t -> int -> 'a
-
-  (** Return the length (number of elements) of the given array *)
-  val length : 'a t -> int
-
-  (** [sub a pos len] returns a fresh array of length [len], containing the
-       elements number [pos] to [pos + len - 1] of array [a].
-  *)
-  val sub : 'a t -> int -> int -> 'a t
-
-  (** [sub_noalloc a pos len] is the same than {!sub} but does not perform
-      allocation. The elements of the output are physically the same than the
-      input. Consequently, modifying the elements of the input will modify the
-      output, and inversely.
-  *)
-  val sub_noalloc : 'a t -> int -> int -> 'a t
-
-  (** [copy a] returns a fresh copy of [a] *)
-  val copy : 'a t -> 'a t
-
-  (** [iter f a] iterates [f] over [a] *)
-  val iter : ('a -> unit) -> 'a t -> unit
-
-  (** Same as {!Array.iter}, but the function is applied to the index of the
-      element as first argument, and the element itself as second argument
-
-      TODO: easy
-  *)
-  (* val iteri : (int -> 'a -> unit) -> 'a array -> unit *)
-
-  (** [for_all f a] checks if all elements of the array satisfy the predicate [f] *)
-  val for_all : ('a -> bool) -> 'a t -> bool
-
-  (* FIXME: mmh, how can you allocate the type 'b t??? *)
-  (* val map : ('a -> 'b) -> 'a t -> 'b t *)
-
-  (** [append v1 v2] returns a fresh array containing the concatenation of the
-      arrays [v1] and [v2].
-
-      Raises [Invalid_argument] if [length v1 + length v2 >
-      Sys.max_array_length]
-
-      TODO: easy
-  *)
-  (* val append : 'a array -> 'a array -> 'a array *)
-
-  (** Same as Array.append, but concatenates a list of arrays
-
-      TODO: easy
-  *)
-  (* val concat : 'a array list -> 'a array *)
-
-  (** [fill a pos len x] modifies the array [a] in place, storing [x] in
-      elements number [pos] to [pos + len - 1].
-
-      Raises [Invalid_argument] if [pos] and [len] do not designate a valid
-      subarray of [a].
-
-      TODO: easy
-  *)
-  (* val fill : 'a array -> int -> int -> 'a -> unit *)
-
-  (** [blit src src_pos dst dst_pos len] copies [len] elements from array [src],
-      starting at element number [src_pos], to array [dst], starting at element
-      number [dst_pos]. It works correctly even if [src] and [dst] are the same array,
-      and the source and destination chunks overlap.
-
-      Raises [Invalid_argument] if [src_pos] and [len] do not designate a valid
-      subarray of [src], or if [dst_pos] and [len] do not designate a valid subarray of
-      [dst].
-
-      TODO: easy
-  *)
-  (* val blit : 'a array -> int -> 'a array -> int -> int -> unit *)
-
-  (** [exists f [|a1; ...; an|]] checks if at least one element of the array
-      satisfies the predicate [f]. That is, it returns [(f a1) || (f a2) || ... ||
-      (f an)].
-
-      TODO: easy
-  *)
-  (* val exists : ('a -> bool) -> 'a array -> bool *)
-
-  (** [mem a set] is [true] if and only if a is structurally equal to an element
-      of [l] (i.e. there is an [x] in [l] such that [compare a x = 0]).
-
-      TODO: not sure it is easy. Might need a compare function in the functor.
-  *)
-  (* val mem : 'a -> 'a array -> bool *)
-end
-
 module Stubs = struct
   type 'a carray
 
   type 'a t = 'a carray * int
+
+  external allocate_carray_elem : int -> 'a = "caml_allocate_carray_elem_stubs"
 
   (* always n followed by size_in_bytes *)
   external allocate_carray : int -> int -> 'a carray
@@ -164,89 +37,109 @@ module Stubs = struct
   external iter : ('a -> unit) -> 'a t -> int -> int -> unit
     = "caml_iter_carray_stubs"
 
-  (* external map : ('a -> 'b) -> 'a t -> 'b t -> int -> int -> int -> unit *)
-  (*   = "caml_map_carray_stubs_bytecode" "caml_map_carray_stubs" *)
+  external iteri : (int -> 'a -> unit) -> 'a t -> int -> int -> unit
+    = "caml_iteri_carray_stubs"
+
+  external map : 'b t -> ('a -> 'b) -> 'a t -> int -> int -> int -> unit
+    = "caml_map_carray_stubs_bytecode" "caml_map_carray_stubs"
+
+  external mem : 'a t -> 'a -> int -> int -> bool = "caml_mem_carray_stubs"
+
+  external exists : 'a t -> ('a -> bool) -> int -> int -> bool
+    = "caml_exists_carray_stubs"
+
+  external append : 'a t -> 'a t -> 'a t -> int -> int -> int -> unit
+    = "caml_append_carray_stubs_bytecode" "caml_append_carray_stubs"
 end
 
-module Make (P : sig
-  type t
+module Generic = struct
+  (* we encode the size in bytes of each element in the type *)
+  type 'a t = 'a Stubs.carray * int * int * int
 
-  val size_in_bytes : int
-
-  (* IMPROVEME/FIXME/COMMENTME: This is only used for get. I'd like to have a better interface.
-     It is not common to have a fresh fn in the interface *)
-  val fresh : unit -> t
-end) =
-struct
-  type 'a t = 'a Stubs.carray * int * int
-
-  let size_in_bytes = P.size_in_bytes
-
-  let allocate n = (Stubs.allocate_carray n P.size_in_bytes, n, 0)
+  let compute_size_in_bytes v =
+    let v = Obj.magic v in
+    if Obj.tag v <> Obj.custom_tag then
+      raise
+      @@ Invalid_argument
+           "Carray: only custom block without out-of-heap values are supported \
+            at the moment" ;
+    (Obj.reachable_words v - 2) * 8
 
   (* FIXME: what about empty list? *)
   let make size v =
+    let size_in_bytes = compute_size_in_bytes v in
     if size < 1 then
       raise @@ Invalid_argument "Carray.make: size be greater than 1" ;
-    (Stubs.make size P.size_in_bytes v, size, 0)
+    (Stubs.make size size_in_bytes v, size, 0, size_in_bytes)
 
-  let set (carray, n, ofs) v i =
+  let set (carray, n, ofs, size_in_bytes) v i =
     if i < ofs || i >= n then
       raise @@ Invalid_argument "Carray.set: index out of bounds" ;
-    Stubs.set (carray, ofs) v i P.size_in_bytes
+    Stubs.set (carray, ofs) v i size_in_bytes
 
-  let get (carray, n, ofs) i =
+  let get (carray, n, ofs, size_in_bytes) i =
     if i < ofs || i >= n then
       raise @@ Invalid_argument "Carray.get: index out of bounds" ;
-    let v = P.fresh () in
-    Stubs.get (Obj.magic v) (carray, ofs) i P.size_in_bytes ;
+    let v = Stubs.allocate_carray_elem size_in_bytes in
+    Stubs.get (Obj.magic v) (carray, ofs) i size_in_bytes ;
     Obj.magic v
 
   let init n f =
     if n < 1 then
       raise @@ Invalid_argument "Carray.init: size be greater than 1" ;
     let ofs = 0 in
-    let carray = Stubs.allocate_carray n P.size_in_bytes in
+    (* we suppose the output of f on 0 exists... It should be as it is used to
+       initialize the array later *)
+    let size_in_bytes = compute_size_in_bytes (f 0) in
+    let carray = Stubs.allocate_carray n size_in_bytes in
     for i = 0 to n - 1 do
-      Stubs.set (carray, ofs) (f i) i P.size_in_bytes
+      Stubs.set (carray, ofs) (f i) i size_in_bytes
     done ;
-    (carray, n, ofs)
+    (carray, n, ofs, size_in_bytes)
 
   let of_array array =
     let ofs = 0 in
     let length = Array.length array in
     if length < 1 then
       raise @@ Invalid_argument "Carray.of_array: size be greater than 1" ;
-    let carray = Stubs.allocate_carray length P.size_in_bytes in
-    Stubs.of_array carray array length P.size_in_bytes ;
-    (carray, length, ofs)
+    let size_in_bytes = compute_size_in_bytes array.(0) in
+    let carray = Stubs.allocate_carray length size_in_bytes in
+    Stubs.of_array carray array length size_in_bytes ;
+    (carray, length, ofs, size_in_bytes)
 
-  let to_array (carray, length, ofs) =
-    let array = Array.init length (fun _ -> Obj.magic P.(fresh ())) in
-    Stubs.to_array array (carray, ofs) length P.size_in_bytes ;
+  let to_array (carray, length, ofs, size_in_bytes) =
+    let array =
+      Array.init length (fun _ ->
+          Obj.magic (Stubs.allocate_carray_elem size_in_bytes))
+    in
+    Stubs.to_array array (carray, ofs) length size_in_bytes ;
     array
 
-  let length (_carray, length, _ofs) = length
+  let length (_carray, length, _ofs, _size_in_bytes) = length
 
   (* FIXME: check out of bounds *)
-  let sub (carray, _n, ofs) offset length =
+  let sub (carray, _n, ofs, size_in_bytes) offset length =
     let ofs' = 0 in
-    let new_carray = Stubs.allocate_carray length P.size_in_bytes in
-    Stubs.sub (new_carray, ofs') (carray, ofs) offset length P.size_in_bytes ;
-    (new_carray, length, ofs')
+    let new_carray = Stubs.allocate_carray length size_in_bytes in
+    Stubs.sub (new_carray, ofs') (carray, ofs) offset length size_in_bytes ;
+    (new_carray, length, ofs', size_in_bytes)
 
   (* FIXME: check out of bounds *)
-  let sub_noalloc (carray, _n, ofs) offset length =
-    (carray, length, ofs + offset)
+  let sub_noalloc (carray, _n, ofs, size_in_bytes) offset length =
+    (carray, length, ofs + offset, size_in_bytes)
 
-  let copy (carray, n, ofs) =
-    let new_carray = Stubs.allocate_carray n P.size_in_bytes in
-    Stubs.copy (carray, ofs) (new_carray, 0) n P.size_in_bytes ;
-    (new_carray, n, 0)
+  let copy (carray, n, ofs, size_in_bytes) =
+    let new_carray = Stubs.allocate_carray n size_in_bytes in
+    Stubs.copy (carray, ofs) (new_carray, 0) n size_in_bytes ;
+    (new_carray, n, 0, size_in_bytes)
 
   (* FIXME: It looks like it is slower than Array.iter when addition
      Bls12_381.Fr points with add_inplace *)
-  let iter f (carray, n, ofs) = Stubs.iter f (carray, ofs) n P.size_in_bytes
+  let iter f (carray, n, ofs, size_in_bytes) =
+    Stubs.iter f (carray, ofs) n size_in_bytes
+
+  let iteri f (carray, n, ofs, size_in_bytes) =
+    Stubs.iteri f (carray, ofs) n size_in_bytes
 
   let to_list a = Array.to_list (to_array a)
 
@@ -257,4 +150,78 @@ struct
     let f' x = res := !res && f x in
     iter f' a ;
     !res
+
+  let map f (input_carray, input_n, input_ofs, input_size_in_bytes) =
+    let init_v =
+      get (input_carray, input_n, input_ofs, input_size_in_bytes) 0
+    in
+    let output_carray, _output_n, output_ofs, output_size_in_bytes =
+      make input_n (f init_v)
+    in
+    Stubs.map
+      (output_carray, output_ofs)
+      f
+      (input_carray, input_ofs)
+      input_n
+      input_size_in_bytes
+      output_size_in_bytes ;
+    (output_carray, input_n, output_ofs, output_size_in_bytes)
+
+  let mem x (carray, n, ofs, size_in_bytes) =
+    Stubs.mem (carray, ofs) x n size_in_bytes
+
+  let exists f (carray, n, ofs, size_in_bytes) =
+    Stubs.exists (carray, ofs) f n size_in_bytes
+
+  let append (carray, n, ofs, size_in_bytes) (carray', n', ofs', _size_in_bytes)
+      =
+    if n + n' > Sys.max_array_length then
+      raise (Invalid_argument "Carray.append: output array too large") ;
+    let v = Stubs.allocate_carray_elem size_in_bytes in
+    let output, output_length, output_ofs, _size_in_bytes =
+      make (n + n') (Obj.magic v)
+    in
+    Stubs.append
+      (output, output_ofs)
+      (carray, ofs)
+      (carray', ofs')
+      n
+      n'
+      size_in_bytes ;
+    (output, output_length, output_ofs, size_in_bytes)
+
+  let concat array_list =
+    if List.length array_list = 0 then
+      raise (Invalid_argument "Carray.concat: empty list given") ;
+    let size = List.fold_left (fun res (_, n, _, _) -> res + n) 0 array_list in
+    let _, _, _, size_in_bytes = List.hd array_list in
+    if size > Sys.max_array_length then
+      raise (Invalid_argument "Carray.append: output array too large") ;
+    let v = Stubs.allocate_carray_elem size_in_bytes in
+    let output, output_length, output_ofs, _ = make size (Obj.magic v) in
+    let current_ofs = ref 0 in
+    List.iter
+      (fun (carray, n, ofs, _) ->
+        Stubs.copy (output, !current_ofs) (carray, ofs) n size_in_bytes ;
+        current_ofs := n + !current_ofs)
+      array_list ;
+    (output, output_length, output_ofs, size_in_bytes)
+
+  let fill (carray, n, ofs, size_in_bytes) pos len v =
+    if pos + len > n then
+      raise (Invalid_argument "Carray.fill: invalid pos or len") ;
+    for i = 0 to len - 1 do
+      Stubs.set (carray, ofs) v (i + pos) size_in_bytes
+    done
+
+  let blit (carray, n, ofs, size_in_bytes) src_pos (carray', n', ofs', _)
+      dst_pos len =
+    if src_pos + len > n || dst_pos + len > n' then
+      raise (Invalid_argument "Carray.blit: invalid src_pos, dst_pos or len") ;
+    for i = 0 to len - 1 do
+      let v = get (carray, n, ofs, size_in_bytes) (src_pos + i) in
+      set (carray', n', ofs', size_in_bytes) v (dst_pos + i)
+    done
 end
+
+include Generic
--- src/carray.mli
+++ src/carray.mli
@@ -1,138 +1,106 @@
-module type S = sig
-  (** The type of C arrays *)
-  type 'a t
+(** The type of C arrays *)
+type 'a t
 
-  val size_in_bytes : int
-
-  (** [allocate n] returns a fresh array of length [n] *)
-  val allocate : int -> 'a t
-
-  (** [make n x] returns a fresh array of length [n], initialized with [x].
+(** [make n x] returns a fresh array of length [n], initialized with [x].
       Each element of the array is a fresh copy of [a]. This is different from
       [Array.make] *)
-  val make : int -> 'a -> 'a t
+val make : int -> 'a -> 'a t
 
-  (** [init n f] returns a fresh array of length [n], with element number [i]
+(** [init n f] returns a fresh array of length [n], with element number [i]
       initialized to the result of [f i]. In other terms, [init n f] tabulates the
       results of [f] applied to the integers [0] to [n-1]. *)
-  val init : int -> (int -> 'a) -> 'a t
+val init : int -> (int -> 'a) -> 'a t
 
-  (** [of_array a] builds a C array with the same content than the Caml array [a] *)
-  val of_array : 'a array -> 'a t
+(** [of_array a] builds a C array with the same content than the Caml array [a] *)
+val of_array : 'a array -> 'a t
 
-  (** [to_array a] returns a Caml array with the same content than the C array [a] *)
-  val to_array : 'a t -> 'a array
+(** [to_array a] returns a Caml array with the same content than the C array [a] *)
+val to_array : 'a t -> 'a array
 
-  (** [of_list l] returns a C array with the same content than the list [l] *)
-  val of_list : 'a list -> 'a t
+(** [of_list l] returns a C array with the same content than the list [l] *)
+val of_list : 'a list -> 'a t
 
-  (** [to_list a] returns a Caml list with the same content than the array [a] *)
-  val to_list : 'a t -> 'a list
+(** [to_list a] returns a Caml list with the same content than the array [a] *)
+val to_list : 'a t -> 'a list
 
-  (** [set a n x] modifies array [a] in place, replacing element number [n] with
+(** [set a n x] modifies array [a] in place, replacing element number [n] with
       [x] *)
-  val set : 'a t -> 'a -> int -> unit
+val set : 'a t -> 'a -> int -> unit
 
-  (** [get a n] gets an returns the element number [n] of the array [a]. The
+(** [get a n] gets an returns the element number [n] of the array [a]. The
       first element has number [0]. The last element has number length [length -
       1]. *)
-  val get : 'a t -> int -> 'a
+val get : 'a t -> int -> 'a
 
-  (** Return the length (number of elements) of the given array *)
-  val length : 'a t -> int
+(** Return the length (number of elements) of the given array *)
+val length : 'a t -> int
 
-  (** [sub a pos len] returns a fresh array of length [len], containing the
+(** [sub a pos len] returns a fresh array of length [len], containing the
        elements number [pos] to [pos + len - 1] of array [a].
   *)
-  val sub : 'a t -> int -> int -> 'a t
+val sub : 'a t -> int -> int -> 'a t
 
-  (** [sub_noalloc a pos len] is the same than {!sub} but does not perform
+(** [sub_noalloc a pos len] is the same than {!sub} but does not perform
       allocation. The elements of the output are physically the same than the
       input. Consequently, modifying the elements of the input will modify the
       output, and inversely.
   *)
-  val sub_noalloc : 'a t -> int -> int -> 'a t
+val sub_noalloc : 'a t -> int -> int -> 'a t
 
-  (** [copy a] returns a fresh copy of [a] *)
-  val copy : 'a t -> 'a t
+(** [copy a] returns a fresh copy of [a] *)
+val copy : 'a t -> 'a t
 
-  (** [iter f a] iterates [f] over [a] *)
-  val iter : ('a -> unit) -> 'a t -> unit
+(** [iter f a] iterates [f] over [a] *)
+val iter : ('a -> unit) -> 'a t -> unit
 
-  (** Same as {!Array.iter}, but the function is applied to the index of the
-      element as first argument, and the element itself as second argument
+(** Same as {!Array.iter}, but the function is applied to the index of the
+    element as first argument, and the element itself as second argument
+*)
+val iteri : (int -> 'a -> unit) -> 'a t -> unit
 
-      TODO: easy
-  *)
-  (* val iteri : (int -> 'a -> unit) -> 'a array -> unit *)
+(** [for_all f a] checks if all elements of the array satisfy the predicate [f] *)
+val for_all : ('a -> bool) -> 'a t -> bool
 
-  (** [for_all f a] checks if all elements of the array satisfy the predicate [f] *)
-  val for_all : ('a -> bool) -> 'a t -> bool
+val map : ('a -> 'b) -> 'a t -> 'b t
 
-  (* FIXME: mmh, how can you allocate the type 'b t??? *)
-  (* val map : ('a -> 'b) -> 'a t -> 'b t *)
+(** [append v1 v2] returns a fresh array containing the concatenation of the
+    arrays [v1] and [v2].
 
-  (** [append v1 v2] returns a fresh array containing the concatenation of the
-      arrays [v1] and [v2].
+    Raises [Invalid_argument] if [length v1 + length v2 >
+    Sys.max_array_length]
+*)
+val append : 'a t -> 'a t -> 'a t
 
-      Raises [Invalid_argument] if [length v1 + length v2 >
-      Sys.max_array_length]
+(** Same as {!append}, but concatenates a list of arrays *)
+val concat : 'a t list -> 'a t
 
-      TODO: easy
-  *)
-  (* val append : 'a array -> 'a array -> 'a array *)
+(** [fill a pos len x] modifies the array [a] in place, storing [x] in
+    elements number [pos] to [pos + len - 1].
 
-  (** Same as Array.append, but concatenates a list of arrays
+    Raises [Invalid_argument] if [pos] and [len] do not designate a valid
+    subarray of [a].
+*)
+val fill : 'a t -> int -> int -> 'a -> unit
 
-      TODO: easy
-  *)
-  (* val concat : 'a array list -> 'a array *)
+(** [blit src src_pos dst dst_pos len] copies [len] elements from array [src],
+    starting at element number [src_pos], to array [dst], starting at element
+    number [dst_pos]. It works correctly even if [src] and [dst] are the same array,
+    and the source and destination chunks overlap.
 
-  (** [fill a pos len x] modifies the array [a] in place, storing [x] in
-      elements number [pos] to [pos + len - 1].
+    Raises [Invalid_argument] if [src_pos] and [len] do not designate a valid
+    subarray of [src], or if [dst_pos] and [len] do not designate a valid subarray of
+    [dst].
+*)
+val blit : 'a t -> int -> 'a t -> int -> int -> unit
 
-      Raises [Invalid_argument] if [pos] and [len] do not designate a valid
-      subarray of [a].
+(** [exists f [|a1; ...; an|]] checks if at least one element of the array
+    satisfies the predicate [f]. That is, it returns [(f a1) || (f a2) || ... ||
+    (f an)].
 
-      TODO: easy
-  *)
-  (* val fill : 'a array -> int -> int -> 'a -> unit *)
+*)
+val exists : ('a -> bool) -> 'a t -> bool
 
-  (** [blit src src_pos dst dst_pos len] copies [len] elements from array [src],
-      starting at element number [src_pos], to array [dst], starting at element
-      number [dst_pos]. It works correctly even if [src] and [dst] are the same array,
-      and the source and destination chunks overlap.
-
-      Raises [Invalid_argument] if [src_pos] and [len] do not designate a valid
-      subarray of [src], or if [dst_pos] and [len] do not designate a valid subarray of
-      [dst].
-
-      TODO: easy
-  *)
-  (* val blit : 'a array -> int -> 'a array -> int -> int -> unit *)
-
-  (** [exists f [|a1; ...; an|]] checks if at least one element of the array
-      satisfies the predicate [f]. That is, it returns [(f a1) || (f a2) || ... ||
-      (f an)].
-
-      TODO: easy
-  *)
-  (* val exists : ('a -> bool) -> 'a array -> bool *)
-
-  (** [mem a set] is [true] if and only if a is structurally equal to an element
-      of [l] (i.e. there is an [x] in [l] such that [compare a x = 0]).
-
-      TODO: not sure it is easy. Might need a compare function in the functor.
-  *)
-  (* val mem : 'a -> 'a array -> bool *)
-end
-
-module Make (P : sig
-  type t
-
-  val size_in_bytes : int
-
-  (* IMPROVEME/FIXME/COMMENTME: This is only used for get. I'd like to have a better interface.
-     It is not common to have a fresh fn in the interface *)
-  val fresh : unit -> t
-end) : S
+(** [mem a set] is [true] if and only if a is structurally equal to an element
+    of [l] (i.e. there is an [x] in [l] such that [compare a x = 0]).
+*)
+val mem : 'a -> 'a t -> bool
